b732d3b55316e8f0946fcbf0919ad62fc8aabf0e
CarloMart88 <carlo.martino1988@gmail.com>
2025-09-02 15:31:56 +0200

    feat: ready to call

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,9 +1,24 @@
 import axios from "axios";
-//importo axios
+import { useState } from "react";
+//importo axios e useState
 
-//Lista di Attrici: [https://lanciweb.github.io/demo/api/actresses/) scelgo di iniziare con la lista attrici
+/* MILESTONE 1
+ Al caricamento dell'applicazione, recuperiamo la lista degli attori e delle attrici dalle API e stampiamoli in console.
+ Lista di Attrici: [https://lanciweb.github.io/demo/api/actresses/) scelgo di iniziare con la lista attrici
+*/
 
 function Main() {
+  // creo la variabile globale per visulaizzare il contenuto della chiamata API
+  const [femaleActors, setFemaleActors] = useState([]);
+
+  const link = "https://lanciweb.github.io/demo/api/actresses/";
+
+  //preparo la funzione
+  const fetchData = () => {
+    axios.get(link).then((resp) => {
+      setFemaleActors(resp.data);
+    });
+  };
   return (
     <div>
       <h1>ok eccoci</h1>
